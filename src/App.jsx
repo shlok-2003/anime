fc8f50619dbe9be1e79aacbd888325949e084ab6
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-10-08 22:34:50 +0530

    Initialized template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import Example from '@/component/Example';
+import Example from '@/components/Example';
+
+export default function App() {
+    return (
+        <div>
+            <h1>App</h1>
+            <Example />
+        </div>
+    );
+}
